af06fcb54ddb818edae494cd51dac46a1945f3a0
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-08-18 20:56:47 +0100

    Packages

--- babel.config.js
+++ babel.config.js
@@ -16,37 +16,12 @@ const presets = [
   ]
 ]
 
-const plugins = [
-  '@babel/proposal-export-default-from',
-  '@babel/proposal-do-expressions',
-  '@babel/proposal-export-namespace-from',
-  '@babel/proposal-throw-expressions',
-  [
-    '@babel/proposal-class-properties',
-    {
-      loose: false
-    }
-  ],
-  [
-    'module-resolver',
-    {
-      root: ['./src'],
-      cwd: 'babelrc',
-      alias: {
-        '@modernpoacher': './src',
-        build: './build'
-      }
-    }
-  ]
-]
-
 module.exports = (api = { cache: { using: () => console.error({ NODE_ENV }) } }) => {
   console.log({ NODE_ENV })
 
   api.cache.using(() => NODE_ENV)
 
   return {
-    presets,
-    plugins
+    presets
   }
 }
